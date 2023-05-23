2ae54a915badb947730cc5ae2dbf044ad9885a71
Bertan2000 <bertanco@abv.bg>
2023-05-23 20:21:27 +0300

    changes

--- project.js
+++ project.js
@@ -7,54 +7,55 @@ requestAnimationFrame([
     "esri/widgets/Home",
     "esri/widgets/LayerList",
     "esri/widgets/BasemapToggle",
-    "esri-widgets/BasemapGallery",
+    "esri/widgets/BasemapGallery",
     "esri/widgets/Search"
-], (esriconfig, WebMap, MapView, ScaleBar, Legend, Home, LayerList, BasemapToggle, BasemapGallery, Search)=>{
-    esriconfig.apikey="AAPK8b13c968054241359f234e78c7b004eefkKbVO9Hk6bBGOoBlPNrS5lFTXkOoZcQ4fo5j0eYOrMTRD_k5EhLbkVobAnOfCv0";
+], (esriconfig, WebMap, MapView, ScaleBar, Legend, Home, LayerList, BasemapToggle, BasemapGallery, Search) => {
+    esriconfig.apikey = "AAPK8b13c968054241359f234e78c7b004eefkKbVO9Hk6bBGOoBlPNrS5lFTXkOoZcQ4fo5j0eYOrMTRD_k5EhLbkVobAnOfCv0";
 
-    const webMap=new WebMap({
-        portalItem:{
-            id:"232b4d297d054b2a831a3ce629ac8495"
+    const webMap = new WebMap({
+        portalItem: {
+            id:
+            "232b4d297d054b2a831a3ce629ac8495"
         }
 
     })
-    const view=new MapView({
-        container:"viewDiv",
-        map:WebMap
+    const view = new MapView({
+        container: "viewDiv",
+        map:webMap
 
     })
-    const homeBtn=new Home({
-        view:view;
+    const homeBtn = new Home({
+        view: view
     })
 
     view.ui.add(homeBtn, "top-left");
 
-    const Legend=new Legend({
-        view:view
+    const legend=new Legend({
+        view: view
 
     })
-    view.ui.add(Legend, "bottom-left");
+    view.ui.add(legend, "bottom-left");
 
-    const scaleBar=new ScaleBar({
-        view:view,
-        unit:"metric",
-        style:"ruler"
+    const scaleBar = new ScaleBar({
+        view: view,
+        unit: "metric",
+        style: "ruler"
     })
 
     view.ui.add(scaleBar, "bottom-right");
 
-    view.ui.add("LayerList-btn", "top-right");
+    view.ui.add("layerList-btn", "top-right");
     view.ui.add("basemap-btn", "top-right");
 
-    const basemapToggle=new BasemapToggle({
-        view:view,
+    const basemapToggle = new BasemapToggle({
+        view: view,
         nextBasemap: "arcgis-imagery"
     })
 
-    const basemapGalley=new BasemapGallery({
-        view:view,
-        source:{
-            query:{
+    const basemapGalley = new BasemapGallery({
+        view: view,
+        source: {
+            query: {
 
             }
         },
@@ -64,44 +65,55 @@ requestAnimationFrame([
 
     view.ui.add(basemapToggle, "bottom-right");
 
-    const LayerList=new LayerList({
-        view:view
+    const LayerList = new LayerList({
+        view: view
     })
 
     view.ui.add(LayerList, "top-right");
 
     document
-    .getElementById("LayerList-btn")
-    .addEventListener("click", function() {
+
+     .getElementById("layerList-btn")
+     .addEventListener("click", function () {
+
         toggleButton("LayerList")
-    })
+     })
 
     document
-    .getElementById("basemap-btn")
-    .getEventListener("click", function(){
-        toggleButton("gallery");
-    })
+     .getElementById("basemap-btn")
+     .addEventListener("click", function (){
 
-    const searchWidget=new Search({
-        view:view
+        toggleButton("gallery");
+     })
+
+    const searchWidget = new Search({
+        view: view
     })
 
     view.ui.add(searchWidget, "top-left");
 
-    function toggleButton(item){
-        const LayerListEl=document.getElementsByClassName("esri-layer-list")[0];
+    function toggleButton(item) {
+        const layerListEl=
+        document.getElementsByClassName("esri-layer-list")[0];
         let currentProp;
-        const galleryEl=document.getElementsByClassName("esri-basemap-galley")[0];
+        const galleryEl = document.getElementsByClassName("esri-basemap-galley")[0];
 
-        if(item=="LayerList"){
-           currentProp=LayerListEl.style.getPropertyValue("display");
-           LayerListEl.style.setProperty("display", currentProp=="block"? "none":"block");
+        if(item == "LayerList") {
+           currentProp = 
+           layerListEl.style.getPropertyValue 
+           ("display");
+           layerListEl.style.setProperty("display", currentProp == "block" ? 
+           "none" : "block");
            galleryEl.style.setProperty("display", "none");
-     } else if(item=="gallery"){
-        currentProp=galleryEl.style.getPropertyValue("display");
-        galleryEl.style.setProperty("display", currentProp=="block"? "none":"block");
-        LayerListEl.style.setProperty("display", "none");
-      }
+      } else if (item == "gallery") {
+          currentProp = 
+          galleryEl.style.getPropertyValue ("display");
+
+        galleryEl.style.setProperty("display", currentProp == "block"?
+          "none" : "block");
+
+        layerListEl.style.setProperty("display", "none");
+       }
     }
 
 })
